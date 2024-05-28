c995f963184002d2d5d6a7c8de142fd1e82a85f9
spector700 <72362399+spector700@users.noreply.github.com>
2024-05-27 21:06:48 -0500

    test 2

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,14 @@
 #include "config/config.h"
 #include "log.h"
 #include <cstdlib>
-#include <iostream>
 
 int main(int argc, char *argv[]) {
 
   auto app = App::create();
 
-  if (!Config::configInit()) {
-    /* exit(1); */
-    std::cout << "Config falied" << '\n';
-  }
+  /* if (!Config::configInit()) { */
+  /*   exit(1); */
+  /* } */
 
   Log::get().setLevel(Log::LevelDebug);
   Log::get().info("Lumastart is now starting...");
