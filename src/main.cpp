f1b681f16d5fbf4a86a5940001cb3b5c7ba81755
spector700 <72362399+spector700@users.noreply.github.com>
2024-05-27 20:33:56 -0500

    testing hyprland segfault

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,15 @@
 #include "config/config.h"
 #include "log.h"
 #include <cstdlib>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
 
   auto app = App::create();
 
   if (!Config::configInit()) {
-    exit(1);
+    /* exit(1); */
+    std::cout << "Config falied" << '\n';
   }
 
   Log::get().setLevel(Log::LevelDebug);
